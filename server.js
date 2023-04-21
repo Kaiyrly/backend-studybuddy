679bf7ebed7616d2818fc5f7ea50a941117a0bb6
Kaiyrly Mukhametkarim <kaiyrly@Kaiyrlys-MacBook-Pro.local>
2023-04-22 01:26:26 +0900

    for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require('path');
 require("dotenv").config();
 const routes = require('./routes');
 
@@ -8,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5001;
 
